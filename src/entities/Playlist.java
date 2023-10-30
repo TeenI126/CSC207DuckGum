8a1c82a2bea9d305b3a3e838e7f03078525b8503
Christopher <christopherchiu271@gmail.com>
2023-10-30 10:39:18 -0400

    Added more entity functions and MusciService entity

--- src/entities/Playlist.java
+++ src/entities/Playlist.java
@@ -5,8 +5,31 @@ import java.util.List;
 public class Playlist {
     private List<Song> songs;
 
+    public Playlist(List<Song> songs) {
+        this.songs = songs;
+    }
     public void addSong(Song song){
         //TODO
         songs.add(song);
+    }
+
+    public void removeSong(Song song) {
+        int n = songs.size();
+        for (int i = 0; i < n; i++) {
+            if (song == songs.get(i)) {
+                songs.remove(i);
+            }
+        }
+    }
+
+    public boolean isPlaylistEmpty() {
+        return songs.isEmpty();
+    }
+
+    public List<Song> getPlaylist() {
+        if (isPlaylistEmpty()) {
+            return null;
+        }
+        return songs;
     }
 }
