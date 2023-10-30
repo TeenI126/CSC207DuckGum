666ea71985ceb54b2d35cc3b24639d7f168eebe3
Gavin Burnham <gavinburnham@outlook.com>
2023-10-30 10:13:16 -0400

    Created an outline for our TransferPlaylistInteractor class, this is following Clean Architecture.

--- src/use_case/TransferPlaylist/TransferPlaylistInteractor.java
+++ src/use_case/TransferPlaylist/TransferPlaylistInteractor.java
@@ -1,4 +1,18 @@
 package use_case.TransferPlaylist;
 
-public class TransferPlaylistInteractor {
+public class TransferPlaylistInteractor implements TransferPlaylistInputBoundary{
+    final TransferPlaylistDataAccessInterface playlistDataAccessInterface;
+    final TransferPlaylistOutputBoundary playlistPresenter;
+
+    public TransferPlaylistInteractor(TransferPlaylistDataAccessInterface playlistDataAccessInterface,
+                                      TransferPlaylistOutputBoundary playlistOutputBoundary){
+        this.playlistDataAccessObject = playlistDataAccessInterface;
+        this.playlistPresenter = playlistOutputBoundary;
+    }
+
+    public void execute(TransferPlaylistInputData playlistInputData){
+
+    }
 }
+
+
