8b890e1c96b988b85f0a21a7a9d4a3dcf8b40957
TeenI126 <mateen.ismail@mail.utoronto.ca>
2023-10-30 10:58:51 -0400

    added get token to SpotifyAccount and started with user auth

--- src/entities/SpotifyAccount.java
+++ src/entities/SpotifyAccount.java
@@ -1,7 +1,45 @@
 package entities;
 
+import okhttp3.*;
+import org.json.*;
+import org.jsoup.Connection;
+
+import java.io.IOException;
+import java.util.Base64;
+
 public class SpotifyAccount extends Account{
     private String clientID = "b273f44d44168fe8c8649e95f86";
+    private String clientSecret = "5bfa9aa652c5461d98fcc235842cbc6c";
+    private final String url = "https://accounts.spotify.com/api/";
+    private String token = null;
+    SpotifyAccount(){
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        Request request = new Request.Builder().url("").addHeader("")
+    }
 
+    private void authorize(){
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        Request request = new Request.Builder().url(url + "authorize")
+    }
 
+    private void updateToken(){
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        RequestBody body = RequestBody.create(mediaType, "grant_type:client_credentials")
+        Request request = new Request.Builder().url(url+"token")
+                .method("POST",body)
+                .addHeader("Authorization", "Basic" + Base64.getEncoder().encodeToString((clientID+":"+clientSecret).getBytes()))
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            JSONObject responseJSON = new JSONObject(response.body().string());
+
+            token = responseJSON.getString("access_token");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
