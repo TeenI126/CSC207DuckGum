13231f778375badbd0bb0e6906c4987d1bf8d8fd
TeenI126 <mateen.ismail@mail.utoronto.ca>
2023-10-30 09:18:54 -0400

    added some more unimplemeted classes to plan program out

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # CSC207DuckGum
 
 using 17 as jdk
+
+## Use Cases
+- sign in to service
+- transfer playlist from one service to another
--- src/entities/Playlist.java
+++ src/entities/Playlist.java
@@ -1,4 +1,11 @@
 package entities;
 
+import java.util.List;
+
 public class Playlist {
+    private List<Song> songs;
+
+    public void addSong(Song song){
+        //TODO
+    }
 }
