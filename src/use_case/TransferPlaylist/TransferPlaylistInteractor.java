017365ceab7519fa5e76c6308d2774087c2c7e85
Gavin Burnham <gavinburnham@outlook.com>
2023-11-20 00:29:13 -0500

    Created a high-level guide to begin to further develop. Mapped out needed methods and I'm going to work on implementing those.

--- src/use_case/TransferPlaylist/TransferPlaylistInteractor.java
+++ src/use_case/TransferPlaylist/TransferPlaylistInteractor.java
@@ -1,17 +1,55 @@
 package use_case.TransferPlaylist;
 
-public class TransferPlaylistInteractor extends TransferPlaylistInputBoundary {
-    final TransferPlaylistDataAccessInterface playlistDataAccessObject;
-    final TransferPlaylistOutputBoundary playlistPresenter;
+import com.spotify.api.SpotifyApi; // Replace with the actual import from the Spotify SDK
+import com.amazon.music.AmazonMusicApi; // Replace with the actual import from the Amazon Music SDK
 
-    public TransferPlaylistInteractor(TransferPlaylistDataAccessInterface playlistDataAccessInterface,
-                                      TransferPlaylistOutputBoundary playlistOutputBoundary){
-        this.playlistDataAccessObject = playlistDataAccessInterface;
-        this.playlistPresenter = playlistOutputBoundary;
+public class TransferPlaylistInteractor {
+
+    private SpotifyApi spotifyApi;
+    private AmazonMusicApi amazonMusicApi;
+
+    public PlaylistInteractor(SpotifyApi spotifyApi, AmazonMusicApi amazonMusicApi) {
+        this.spotifyApi = spotifyApi;
+        this.amazonMusicApi = amazonMusicApi;
     }
 
-    public void execute(TransferPlaylistInputData playlistInputData){
+    public void transferPlaylistFromSpotifyToAmazon(String spotifyPlaylistId) {
+        Playlist spotifyPlaylist = fetchPlaylistFromSpotify(spotifyPlaylistId);
+        Playlist amazonPlaylist = createPlaylistOnAmazon(spotifyPlaylist.getName());
+        addSongsToAmazonPlaylist(spotifyPlaylist, amazonPlaylist);
+    }
 
+    public void transferPlaylistFromAmazonToSpotify(String spotifyPlaylistId) {
+        Playlist amazonPlaylist = fetchPlaylistFromAmazon(spotifyPlaylistId);
+        Playlist spotifyPlaylist = createPlaylistOnSpotify(spotifyPlaylist.getName());
+        addSongsToSpotifyPlaylist(amazonPlaylist, spotifyPlaylist);
+    }
+
+
+    private Playlist fetchPlaylistFromSpotify(String playlistId) {
+        // Implementation to fetch playlist details and songs from Spotify
+    }
+
+    private Playlist fetchPlaylistFromAmazon(String playlistId) {
+        // Implementation to fetch playlist details and songs from Amazon
+    }
+
+    private Playlist createPlaylistOnAmazon(String playlistName) {
+        // Implementation to create a new playlist on Amazon Music
+    }
+
+    private Playlist createPlaylistOnSpotify(String playlistName) {
+        // Implementation to create a new playlist on Spotify
+    }
+
+    private void addSongsToAmazonPlaylist(Playlist spotifyPlaylist, Playlist amazonPlaylist) {
+        // Map and add songs from Spotify playlist to Amazon playlist
+        // Song mapping is a complex process due to different song IDs on platforms
+    }
+
+    private void addSongsToSpotifyPlaylist(Playlist amazonPlaylist, Playlist spotifyPlaylist) {
+        // Map and add songs from Amazon playlist to Spotify playlist
+        // Song mapping is a complex process due to different song IDs on platforms
     }
 }
 
