790e8bb98e59f009cd9cd8f47589fc7ca8f865da
Daniel Makarov <danielmakarov.dev@gmail.com>
2023-11-22 15:31:19 -0500

    Created a feature where playslists can be added to Amazon music

--- src/use_case/TransferPlaylist/TransferPlaylistInputData.java
+++ src/use_case/TransferPlaylist/TransferPlaylistInputData.java
@@ -1,5 +1,10 @@
 package use_case.TransferPlaylist;
 
+import okhttp3.*;
+import entities.Account;
+import entities.Playlist;
+import java.io.IOException;
+
 public class TransferPlaylistInputData {
 
     final private Playlist playlist;
@@ -12,9 +17,33 @@ public class TransferPlaylistInputData {
         this.toAccount = toAccount;
     }
 
-    Playlist getPlaylist() {return playlist; }
+    Playlist getPlaylist() { return playlist; }
 
-    Account getFromAccount() {return fromAccount; }
-    Account getToAccount() {return toAccount; }
+    Account getFromAccount() { return fromAccount; }
 
+    // Updates Amazon Music Playlist
+    Account getToAccount() {
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://api.music.amazon.dev/v1/playlists/4a5afe86-7316";
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        RequestBody body = RequestBody.create(mediaType,
+                "title=One of these days&description=A day of Pink Floyd&visibility=PUBLIC");
+        Request request = new Request.Builder()
+                .url(url)
+                .put(body)
+                .addHeader("x-api-key", "<your client ID>")
+                .addHeader("Authorization", "Bearer <your auth token>")
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            // Handle the response as needed
+            // For example, you could log the response, check for success status, etc.
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle the exception as needed
+        }
+
+        return toAccount;
+    }
 }
