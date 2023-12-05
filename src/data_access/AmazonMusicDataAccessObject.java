55cbba85e8184a3b5f4c6e18d581f2ecec74698a
Daniel Makarov <danielmakarov.dev@gmail.com>
2023-12-05 16:46:41 -0500

    files updated correctly

--- src/data_access/AmazonMusicDataAccessObject.java
+++ src/data_access/AmazonMusicDataAccessObject.java
@@ -6,76 +6,146 @@ import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
 import java.net.URL;
 import org.json.JSONObject;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
-import java.io.IOException;
-/**
- * Handles data access operations for Amazon Music.
- * Responsible for authenticating users and fetching user-related data from Amazon Music API.
- */
 public class AmazonMusicDataAccessObject {
 
-    private OkHttpClient client;
-    private String apiKey;
-    private String authToken;
-    private String baseUrl;
+    // Client credentials and redirect URI
+    private static final String CLIENT_ID = "amzn1.application-oa2-client.951516002f594c19922fd8aa22fa93fc";
+    private static final String CLIENT_SECRET = "amzn1.oa2-cs.v1.2aa4cb84b16e1d32dbc64b4c686e956188c875fb24f9d5f896426d9301fb6684";
+    private static final String REDIRECT_URI = "http://localhost:8080/callback";
 
-    public AmazonAccount(String apiKey, String authToken, String baseUrl) {
-        this.client = new OkHttpClient();
-        this.apiKey = apiKey;
-        this.authToken = authToken;
-        this.baseUrl = baseUrl;
+    // Variables to store token and user information
+    private static String accessToken = "";
+    private static String refreshToken = "";
+    private static String userId = "";
+    private static String userName = "";
+    private static String userEmail = "";
+
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/callback", exchange -> {
+            String query = exchange.getRequestURI().getQuery();
+            String responseText = "Authorization code not found.";
+            if (query != null && query.contains("code=")) {
+                String authorizationCode = query.substring(query.indexOf("code=") + 5);
+                JSONObject tokenJson = new JSONObject(exchangeAuthorizationCode(authorizationCode));
+                accessToken = tokenJson.getString("access_token");
+                refreshToken = tokenJson.optString("refresh_token");
+
+                String userProfile = fetchUserProfile(accessToken);
+                String userPlaylistsJson = fetchUserPlaylists(accessToken);
+                responseText = "User Profile: " + userProfile + "\n\nPlaylists: " + userPlaylistsJson;
+
+                // Update GUI with user profile and playlists
+                AmazonMusicDataAccessObject.updateUserInfo(userProfile, userPlaylistsJson);
+
+                // Prepare response text for the web
+                String responseTextx = "Login Successful!\n\nUser Profile:\n" + userProfile + "\n\nPlaylists:\n" + userPlaylistsJson;
+                exchange.sendResponseHeaders(200, responseTextx.length());
+                OutputStream os = exchange.getResponseBody();
+                os.write(responseTextx.getBytes());
+                os.close();
+            }
+
+            exchange.sendResponseHeaders(200, responseText.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(responseText.getBytes());
+            System.out.println(responseText);
+            os.close();
+        });
+
+        server.start();
+        System.out.println("Server started on port 8080. Navigate to http://localhost:8080/callback to see it in action.");
     }
 
-    public String getCurrentUser() throws IOException {
-        Request request = new Request.Builder()
-                .url(baseUrl + "/v1/me")
-                .get()
-                .addHeader("x-api-key", apiKey)
-                .addHeader("Authorization", "Bearer " + authToken)
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        }
-    }
-
-    public String getUserPlaylists(String playlistId) throws IOException {
-        Request request = new Request.Builder()
-                .url(baseUrl + "/v1/playlists/" + playlistId)
-                .get()
-                .addHeader("x-api-key", apiKey)
-                .addHeader("Authorization", "Bearer " + authToken)
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        }
-    }
-
-    // Add a main method if you want to run this class as a standalone Java application
-    public static void main(String[] args) {
-        // Example usage
-        AmazonAccount account = new AmazonAccount("<your client ID>", "<your auth token>", "https://api.music.amazon.dev");
+    private static String fetchUserProfile(String accessToken) {
         try {
-            String currentUser = account.getCurrentUser();
-            System.out.println("Current User: " + currentUser);
+            URL url = new URL("https://api.amazon.com/user/profile");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Authorization", "Bearer " + accessToken);
 
-            String playlists = account.getUserPlaylists("4a5afe86-7316");
-            System.out.println("User Playlists: " + playlists);
-        } catch (IOException e) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            JSONObject userProfileJson = new JSONObject(readAll(br));
+            userId = userProfileJson.getString("user_id");
+            userName = userProfileJson.getString("name");
+            userEmail = userProfileJson.getString("email");
+        } catch (Exception e) {
             e.printStackTrace();
         }
+        return accessToken;
     }
 
+    private static String fetchUserPlaylists(String accessToken) {
+        try {
+            URL url = new URL("https://api.music.amazon.com/v1/me/playlists");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Authorization", "Bearer " + accessToken);
 
-    // Test Code
-    // Define the client credentials and redirect URI for Amazon API
-//    private static final String CLIENT_ID = "amzn1.application-oa2-client.951516002f594c19922fd8aa22fa93fc";
-//    private static final String CLIENT_SECRET = "amzn1.oa2-cs.v1.2aa4cb84b16e1d32dbc64b4c686e956188c875fb24f9d5f896426d9301fb6684";
-//    private static final String REDIRECT_URI = "http://localhost:8080/callback";
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            return readAll(br);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static String readAll(BufferedReader reader) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+        }
+        return sb.toString();
+    }
+
+    private static String exchangeAuthorizationCode(String authorizationCode) {
+        try {
+            URL url = new URL("https://api.amazon.com/auth/o2/token");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setDoOutput(true);
+
+            String params = String.format("grant_type=authorization_code&code=%s&client_id=%s&client_secret=%s&redirect_uri=%s",
+                    authorizationCode, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+            OutputStream os = connection.getOutputStream();
+            os.write(params.getBytes());
+            os.close();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                response.append(line);
+            }
+            br.close();
+
+            return response.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
+
+
+// Test Code
+//import com.sun.net.httpserver.HttpServer;
+//        import java.io.*;
+//        import java.net.HttpURLConnection;
+//        import java.net.InetSocketAddress;
+//        import java.net.URL;
+//        import org.json.JSONObject;
+//
+//public class RedirectUriServer {
+//
+//    private static final String CLIENT_ID = "Null";
+//    private static final String CLIENT_SECRET = "Null";
+//    private static final String REDIRECT_URI = "Null";
 //
 //    // Variables to store token and user information
 //    private static String accessToken = "";
@@ -85,107 +155,56 @@ public class AmazonMusicDataAccessObject {
 //    private static String userEmail = "";
 //
 //    public static void main(String[] args) throws IOException {
+//        startServer();
+//    }
+//
+//    private static void startServer() throws IOException {
 //        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-//
-//        server.createContext("/callback", exchange -> {
-//            String query = exchange.getRequestURI().getQuery();
-//            String responseText = "Authorization code not found.";
-//            if (query != null && query.contains("code=")) {
-//                String authorizationCode = query.substring(query.indexOf("code=") + 5);
-//                JSONObject tokenJson = new JSONObject(exchangeAuthorizationCode(authorizationCode));
-//                accessToken = tokenJson.getString("access_token");
-//                refreshToken = tokenJson.optString("refresh_token");
-//
-//                String userProfile = fetchUserProfile(accessToken);
-//                String userPlaylistsJson = fetchUserPlaylists(accessToken);
-//                responseText = "User Profile: " + userProfile + "\n\nPlaylists: " + userPlaylistsJson;
-//
-//                // Update GUI with user profile and playlists
-//                AmazonLoginApp.updateUserInfo(responseText);
-//            }
-//
-//            exchange.sendResponseHeaders(200, responseText.length());
-//            OutputStream os = exchange.getResponseBody();
-//            os.write(responseText.getBytes());
-//            os.close();
-//        });
-//
+//        server.createContext("/callback", RedirectUriServer::handleCallbackRequest);
 //        server.start();
-//        System.out.println("Server started on port 8080. Navigate to http://localhost:8080/callback to see it in action.");
+//        System.out.println("Server running on port 8080.");
 //    }
 //
-//    // Method to fetch the user profile using the access token
-//    private static String fetchUserProfile(String accessToken) {
-//        try {
-//            URL url = new URL("https://api.amazon.com/user/profile");
-//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//            connection.setRequestMethod("GET");
-//            connection.setRequestProperty("Authorization", "Bearer " + accessToken);
+//    private static void handleCallbackRequest(HttpExchange exchange) throws IOException {
+//        String query = exchange.getRequestURI().getQuery();
+//        String responseText = processCallbackQuery(query);
 //
-//            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//            JSONObject userProfileJson = new JSONObject(readAll(br));
-//            userId = userProfileJson.getString("user_id");
-//            userName = userProfileJson.getString("name");
-//            userEmail = userProfileJson.getString("email");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return accessToken;
-//    }
-//
-//    // Method to fetch the user's playlists
-//    private static String fetchUserPlaylists(String accessToken) {
-//        try {
-//            URL url = new URL("https://api.music.amazon.com/v1/me/playlists");
-//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//            connection.setRequestMethod("GET");
-//            connection.setRequestProperty("Authorization", "Bearer " + accessToken);
-//
-//            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//            return readAll(br);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return null;
+//        exchange.sendResponseHeaders(200, responseText.getBytes().length);
+//        try (OutputStream os = exchange.getResponseBody()) {
+//            os.write(responseText.getBytes());
 //        }
 //    }
 //
-//    // Helper method to read all data from a BufferedReader instance
-//    private static String readAll(BufferedReader reader) throws IOException {
-//        StringBuilder sb = new StringBuilder();
-//        String line;
-//        while ((line = reader.readLine()) != null) {
-//            sb.append(line);
+//    private static String processCallbackQuery(String query) {
+//        if (query != null && query.contains("code=")) {
+//            String code = extractCodeFromQuery(query);
+//            JSONObject tokenJson = exchangeAuthCodeForToken(code);
+//            updateTokenInfo(tokenJson);
+//
+//            String userProfileInfo = fetchUserProfileInfo(accessToken);
+//            String userPlaylistInfo = fetchUserPlaylistInfo(accessToken);
+//
+//            String fullResponse = "User Profile: " + userProfileInfo + "\n\nPlaylists: " + userPlaylistInfo;
+//            AmazonLoginApp.updateUserInfo(fullResponse);
+//
+//            return fullResponse;
 //        }
-//        return sb.toString();
+//        return "Authorization code not found.";
 //    }
 //
-//    // Method to exchange authorization code for an access token
-//    private static String exchangeAuthorizationCode(String authorizationCode) {
-//        try {
-//            URL url = new URL("https://api.amazon.com/auth/o2/token");
-//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//            connection.setRequestMethod("POST");
-//            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-//            connection.setDoOutput(true);
-//
-//            String params = String.format("grant_type=authorization_code&code=%s&client_id=%s&client_secret=%s&redirect_uri=%s",
-//                    authorizationCode, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
-//            OutputStream os = connection.getOutputStream();
-//            os.write(params.getBytes());
-//            os.close();
-//
-//            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//            StringBuilder response = new StringBuilder();
-//            String line;
-//            while ((line = br.readLine()) != null) {
-//                response.append(line);
-//            }
-//            br.close();
-//
-//            return response.toString();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return null;
-//        }
+//    private static String extractCodeFromQuery(String query) {
+//        return query.substring(query.indexOf("code=") + 5);
 //    }
-}
+//
+//    private static JSONObject exchangeAuthCodeForToken(String authorizationCode) {
+//        String tokenResponse = exchangeAuthorizationCode(authorizationCode);
+//        return new JSONObject(tokenResponse);
+//    }
+//
+//    private static void updateTokenInfo(JSONObject tokenJson) {
+//        accessToken = tokenJson.getString("access_token");
+//        refreshToken = tokenJson.optString("refresh_token");
+//    }
+//
+//    // Remaining methods (fetchUserProfile, fetchUserPlaylists, readAll, exchangeAuthorizationCode) remain the same
+//}
