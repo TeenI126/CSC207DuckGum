94bf2259aedb56562c9add7ad4778569109fd6a7
Gavin Burnham <gavinburnham@outlook.com>
2023-12-05 20:57:23 -0500

    Reworked the TransferPlaylistInteractor, it now calls a transferServiceToService method rather than specifying Amazon to Spotify and vice versa, now you can create adapters for additional services you would like to include by implementing an abstract MusicServiceAdapter interface and making calls on those.

--- src/use_case/TransferPlaylist/TransferPlaylistInteractor.java
+++ src/use_case/TransferPlaylist/TransferPlaylistInteractor.java
@@ -1,89 +1,84 @@
 package use_case.TransferPlaylist;
 
-import data_access.AmazonMusicDataAccessObject;
-import data_access.SpotifyDataAccessObject;
-import entities.Playlist;
-import data_access.*;
 import entities.*;
-import java.io.IOException;
 
 public class TransferPlaylistInteractor implements TransferPlaylistInputBoundary {
 
-    private SpotifyDataAccessObject spotifyDAO;
-    private AmazonMusicDataAccessObject amazonDAO;
+    private MusicServiceAdapter spotifyAdapter;
+    private MusicServiceAdapter amazonAdapter;
     private TransferPlaylistOutputBoundary outputBoundary;
 
-    public TransferPlaylistInteractor(SpotifyDataAccessObject spotifyDAO,
-                                      AmazonMusicDataAccessObject amazonDAO,
+    public TransferPlaylistInteractor(MusicServiceAdapter spotifyAdapter,
+                                      MusicServiceAdapter amazonAdapter,
                                       TransferPlaylistOutputBoundary outputBoundary) {
-        this.spotifyDAO = spotifyDAO;
-        this.amazonDAO = amazonDAO;
+        this.spotifyAdapter = spotifyAdapter;
+        this.amazonAdapter = amazonAdapter;
         this.outputBoundary = outputBoundary;
     }
 
     @Override
     public void transferPlaylist(TransferPlaylistInputData inputData) {
         try {
-            Playlist playlistToTransfer;
             if (inputData.getSourceService().equalsIgnoreCase("Spotify")) {
-                playlistToTransfer = spotifyDAO.getPlaylist(inputData.getSpotifyAccessToken(), inputData.getPlaylistId());
-                // Convert Spotify playlist to Amazon format and create it
-                amazonDAO.createPlaylist(playlistToTransfer, inputData.getAmazonAccessToken());
-                outputBoundary.presentTransferResult(new TransferPlaylistOutputData(true, "Playlist transferred from Spotify to Amazon Music successfully."));
+                transferServiceToService(spotifyAdapter, amazonAdapter, inputData);
             } else if (inputData.getSourceService().equalsIgnoreCase("Amazon")) {
-                playlistToTransfer = amazonDAO.getPlaylist(inputData.getAmazonAccessToken(), inputData.getPlaylistId());
-                // Convert Amazon playlist to Spotify format and create it
-                String userId = spotifyDAO.getCurrentUserId(inputData.getSpotifyAccessToken());
-                spotifyDAO.createPlaylist(userId, playlistToTransfer, inputData.getSpotifyAccessToken());
-                outputBoundary.presentTransferResult(new TransferPlaylistOutputData(true, "Playlist transferred from Amazon Music to Spotify successfully."));
+                transferServiceToService(amazonAdapter, spotifyAdapter, inputData);
             } else {
                 throw new IllegalArgumentException("Unsupported music service for transfer.");
             }
-        } catch (IOException | IllegalArgumentException e) {
+        } catch (Exception e) {
             outputBoundary.presentTransferResult(new TransferPlaylistOutputData(false, "Failed to transfer playlist: " + e.getMessage()));
         }
     }
 
-    // Transfer from Spotify to Amazon
-    private void transferFromSpotifyToAmazon(String spotifyAccessToken, String playlistId, String amazonAccessToken) throws IOException {
-        Playlist spotifyPlaylist = spotifyDAO.getPlaylist(spotifyAccessToken, playlistId);
-        List<Song> mappedSongs = mapSongsForAmazon(spotifyPlaylist.getSongs());
-        Playlist amazonPlaylist = createAmazonPlaylist(mappedSongs, spotifyPlaylist.getName(), amazonAccessToken);
-        amazonDAO.createPlaylist(amazonPlaylist, amazonAccessToken);
+    private void transferServiceToService(MusicServiceAdapter fromAdapter, MusicServiceAdapter toAdapter, TransferPlaylistInputData inputData) throws Exception {
+        Playlist localPlaylist = fromAdapter.fetchPlaylist(inputData.getSourceAccessToken(), inputData.getPlaylistId());
+        toAdapter.createPlaylist(localPlaylist, inputData.getTargetAccessToken());
+        outputBoundary.presentTransferResult(new TransferPlaylistOutputData(true, "Playlist transferred successfully."));
+    }
+}
+
+// Adapter interface
+interface MusicServiceAdapter {
+    Playlist fetchPlaylist(String accessToken, String playlistId) throws Exception;
+    void createPlaylist(Playlist playlist, String accessToken) throws Exception;
+}
+
+// Implementation of the Spotify Adapter
+class SpotifyAdapter implements MusicServiceAdapter {
+    private SpotifyDataAccessObject spotifyDAO;
+    
+    public SpotifyAdapter(SpotifyDataAccessObject spotifyDAO) {
+        this.spotifyDAO = spotifyDAO;
     }
 
-    // Transfer from Amazon to Spotify
-    private void transferFromAmazonToSpotify(String amazonAccessToken, String playlistId, String spotifyAccessToken) throws IOException {
-        Playlist amazonPlaylist = amazonDAO.getPlaylist(amazonAccessToken, playlistId);
-        List<Song> mappedSongs = mapSongsForSpotify(amazonPlaylist.getSongs());
-        Playlist spotifyPlaylist = createSpotifyPlaylist(mappedSongs, amazonPlaylist.getName(), spotifyAccessToken);
-        String userId = spotifyDAO.getCurrentUserId(inputData.getSpotifyAccessToken());
-        spotifyDAO.createPlaylist(userId, playlistToTransfer, inputData.getSpotifyAccessToken());
+    @Override
+    public Playlist fetchPlaylist(String accessToken, String playlistId) throws Exception {
+        return spotifyDAO.getPlaylist(accessToken, playlistId);
     }
 
-    // Map songs from a Spotify playlist for Amazon Music
-    private List<Song> mapSongsForAmazon(List<Song> spotifySongs) {
-        List<Song> amazonSongs = new ArrayList<>();
-        for (Song spotifySong : spotifySongs) {
-            // Implement song mapping logic here
-            Song amazonSong = findSongOnAmazon(spotifySong); // This is a placeholder for the actual mapping logic
-            if (amazonSong != null) {
-                amazonSongs.add(amazonSong);
-            }
-        }
-        return amazonSongs;
+    @Override
+    public void createPlaylist(Playlist playlist, String accessToken) throws Exception {
+        String userId = spotifyDAO.getCurrentUserId(accessToken);
+        spotifyDAO.createPlaylist(userId, playlist, accessToken);
+    }
+}
+
+// Implementation of the Amazon Adapter
+class AmazonAdapter implements MusicServiceAdapter {
+    private AmazonMusicDataAccessObject amazonDAO;
+    
+    public AmazonAdapter(AmazonMusicDataAccessObject amazonDAO) {
+        this.amazonDAO = amazonDAO;
     }
 
-    // Map songs from an Amazon playlist for Spotify
-    private List<Song> mapSongsForSpotify(List<Song> amazonSongs) {
-        List<Song> spotifySongs = new ArrayList<>();
-        for (Song amazonSong : amazonSongs) {
-            // Implement song mapping logic here
-            Song spotifySong = findSongOnSpotify(amazonSong); // This is a placeholder for the actual mapping logic
-            if (spotifySong != null) {
-                spotifySongs.add(spotifySong);
-            }
-        }
-        return spotifySongs;
+    @Override
+    public Playlist fetchPlaylist(String accessToken, String playlistId) throws Exception {
+        return amazonDAO.getPlaylist(accessToken, playlistId);
+    }
+
+    @Override
+    public void createPlaylist(Playlist playlist, String accessToken) throws Exception {
+        amazonDAO.createPlaylist(playlist, accessToken);
     }
 }
